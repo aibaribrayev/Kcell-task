367831785bf0b406032478e8a534f2dae4540c89
AibarIbray <aibar.s.ibrayev@gmail.com>
2023-06-21 08:28:10 +0600

    docker

--- ecom/settings.py
+++ ecom/settings.py
@@ -86,10 +86,13 @@ TEMPLATES: List[Dict[str, Any]] = [{
 
 # Database
 # region
-DATABASES: Dict[str, Dict[str, Any]] = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 # endregion
